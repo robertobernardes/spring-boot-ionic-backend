7f9b61f1891ca7f6ea05b0c8d034f09e8961b48f
Roberto Bernardes <roberto_bernardes@hotmail.com>
2019-09-12 16:19:49 -0300

    Configuração e envio do projeto para produção

--- src/main/java/com/melioalves/cursomc/domain/Categoria.java
+++ src/main/java/com/melioalves/cursomc/domain/Categoria.java
@@ -9,8 +9,10 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
+import javax.persistence.Table;
 
 @Entity
+@Table(name= "categoria", schema = "teste")
 public class Categoria implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
